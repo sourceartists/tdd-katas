db8d7a0ad0a64c6412215d7a3c608178812c43fd
Maciej Kowalski <maciej.kowalski@sourceartists.co.uk>
2016-11-28 18:39:28 +0000

    initial commit

---
+++ src/main/java/co/uk/sourceartists/tddkata/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,7 @@
+package co.uk.sourceartists.tddkata.fizzbuzz;
+
+/**
+ * Created by maciek on 11/28/16.
+ */
+public class FizzBuzz {
+}
---
+++ src/test/java/co/uk/sourceartists/tddkata/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package co.uk.sourceartists.tddkata.fizzbuzz;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+
+/**
+ * Created by maciek on 11/28/16.
+ */
+public class FizzBuzzTest {
+}
